9ded8ba813d7787895f2c0bf1e3619a9dc11491d
sofeeka <sofiiahrychukh@gmail.com>
2024-05-12 21:27:42 +0200

    Updated SectionDTO

--- lib/models/section_dto.dart
+++ lib/models/section_dto.dart
@@ -3,12 +3,45 @@ import 'package:flutter/cupertino.dart';
 class SectionDTO {
   SectionDTO({
     required this.id,
-    required this.color,
     required this.name,
+    required this.color,
     this.iconData,
     this.svgIconPath,
   }) {
     assert(iconData != null || svgIconPath != null);
+  }
+
+  //todo optional add iconData
+  SectionDTO.fromJson(Map<String, Object?> json)
+      : this(
+    id: json['id']! as int,
+    name: json['name']! as String,
+    color: Color(json['color']! as int),
+    svgIconPath: json['svgIconPath']! as String,
+  );
+
+  SectionDTO copyWith({
+    int? id,
+    String? name,
+    Color? color,
+    IconData? iconData,
+    String? svgIconPath,
+  }) {
+    return SectionDTO(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        color: color ?? this.color,
+        iconData: iconData ?? this.iconData,
+        svgIconPath: svgIconPath ?? this.svgIconPath);
+  }
+
+  Map<String, Object?> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'color': color.value,
+      'svgIconPath': svgIconPath,
+    };
   }
 
   final int id;
