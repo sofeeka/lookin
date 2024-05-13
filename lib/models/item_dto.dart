db4a23aed3c9b3a6ace43708db4439ebe8ba02f3
sofeeka <sofiiahrychukh@gmail.com>
2024-05-13 03:49:50 +0200

    Added LookDTO and ItemDTO

---
+++ lib/models/item_dto.dart
@@ -0,0 +1,33 @@
+class ItemDTO {
+  ItemDTO({
+    required this.id,
+    required this.imagePath,
+  });
+
+  ItemDTO.fromJson(Map<String, Object?> json)
+      : this(
+    id: json['id']! as int,
+    imagePath: json['imagePath']! as String,
+  );
+
+  ItemDTO copyWith({
+    int? id,
+    String? imagePath,
+  }) {
+    return ItemDTO(
+        id: id ?? this.id,
+        imagePath: imagePath ?? this.imagePath
+    );
+  }
+
+  //todo optional add iconData
+  Map<String, Object?> toJson() {
+    return {
+      'id': id,
+      'imagePath' : imagePath,
+    };
+  }
+
+  final int id;
+  final String imagePath;
+}
---
+++ lib/models/look_dto.dart
@@ -0,0 +1,45 @@
+import 'item_dto.dart';
+
+class LookDTO {
+  LookDTO({
+    required this.id,
+    this.name,
+    required this.items,
+  });
+
+  factory LookDTO.fromJson(Map<String, Object?> json) {
+    var itemsJson = json['items'] as List<dynamic>;
+    List<ItemDTO> items = itemsJson.map((itemJson) => ItemDTO.fromJson(itemJson)).toList();
+
+    return LookDTO(
+      id: json['id']! as int,
+      name: json['name'] as String?,
+      items: items,
+    );
+  }
+
+  LookDTO copyWith({
+    int? id,
+    String? name,
+    List<ItemDTO>? items,
+  }) {
+    return LookDTO(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        items: items ?? this.items);
+  }
+
+  Map<String, Object?> toJson() {
+    List<Map<String, Object?>> itemsJson = items.map((item) => item.toJson()).toList();
+
+    return {
+      'id': id,
+      'name': name,
+      'items': itemsJson,
+    };
+  }
+
+  final int id;
+  final String? name;
+  final List<ItemDTO> items;
+}
