e60c506e3011a462bb1df97e3cb34eafa52c4535
sofeeka <sofiiahrychukh@gmail.com>
2024-05-16 15:11:36 +0200

    Loaded an image

--- lib/widgets/clothing_item_widget.dart
+++ lib/widgets/clothing_item_widget.dart
@@ -1,16 +1,18 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:lookin_empat/models/item_dto.dart';
 import 'package:lookin_empat/repositories/firestore_item_repository.dart';
 
+import '../models/logger.dart';
 import '../style/colors.dart';
 
 class ClothingItemWidget extends StatelessWidget {
   const ClothingItemWidget({
-    super.key,
+    Key? key,
     required this.sectionId,
     required this.itemDTO,
     required this.firestoreItemRepository,
-  });
+  }) : super(key: key);
 
   final int sectionId;
   final ItemDTO itemDTO;
@@ -25,7 +27,7 @@ class ClothingItemWidget extends StatelessWidget {
       child: SizedBox(
         height: 100,
         width: 100,
-        child: Text(itemDTO.imagePath),
+        child: Image.network(itemDTO.imagePath),
       ),
     );
   }
